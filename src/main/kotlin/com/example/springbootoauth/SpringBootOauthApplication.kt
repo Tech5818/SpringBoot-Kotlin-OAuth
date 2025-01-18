616ca91cbf31f4848735ff95a586566ceafb4ccf
Tech5818 <pqow5818@gmail.com>
2025-01-18 22:22:38 +0900

    Init: Project Init

---
+++ src/main/kotlin/com/example/springbootoauth/SpringBootOauthApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootoauth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootOauthApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootOauthApplication>(*args)
+}
